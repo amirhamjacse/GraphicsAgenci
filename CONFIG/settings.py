a9c6fdfa492784610860e52ce6f42ff745f84076
amirhamjacse <amirhamza824@gmail.com>
2025-08-04 12:18:34 +0600

    updated setting for local

--- CONFIG/settings.py
+++ CONFIG/settings.py
@@ -178,3 +178,13 @@ STATIC_ROOT = '/home/designsc/public_html/static'
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/home/designsc/public_html/media'
+
+
+##### use for development on local #####
+
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
